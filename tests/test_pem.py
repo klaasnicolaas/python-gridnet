ca70ab82a7f4f1380c66062db95dae8ea93385d8
Klaas Schoute <klaas_schoute@hotmail.com>
2021-10-20 20:00:29 +0200

    Add tests for this project

---
+++ tests/test_pem.py
@@ -0,0 +1,114 @@
+"""Basic tests for the Pure Energie device."""
+import asyncio
+from unittest.mock import patch
+
+import aiohttp
+import pytest
+
+from pure_energie import PureEnergie
+from pure_energie.exceptions import (
+    PureEnergieMeterConnectionError,
+    PureEnergieMeterError,
+)
+
+from . import load_fixtures
+
+
+@pytest.mark.asyncio
+async def test_timeout(aresponses):
+    """Test request timeout from Pure Energie."""
+    # Faking a timeout by sleeping
+    async def response_handler(_):
+        await asyncio.sleep(0.2)
+        return aresponses.Response(
+            body="Goodmorning!", text=load_fixtures("smartmeter.json")
+        )
+
+    aresponses.add("example.com", "/meter/now", "GET", response_handler)
+
+    async with aiohttp.ClientSession() as session:
+        client = PureEnergie(host="example.com", session=session, request_timeout=0.1)
+        with pytest.raises(PureEnergieMeterConnectionError):
+            assert await client.smartmeter()
+
+
+@pytest.mark.asyncio
+async def test_client_error():
+    """Test request client error from Pure Energie."""
+    async with aiohttp.ClientSession() as session:
+        client = PureEnergie(host="example.com", session=session)
+        with patch.object(
+            session, "request", side_effect=aiohttp.ClientError
+        ), pytest.raises(PureEnergieMeterConnectionError):
+            assert await client.request("test")
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("status", [401, 403])
+async def test_http_error401(aresponses, status):
+    """Test HTTP 401 response handling."""
+    aresponses.add(
+        "example.com",
+        "/meter/now",
+        "GET",
+        aresponses.Response(text="Give me energy!", status=status),
+    )
+
+    async with aiohttp.ClientSession() as session:
+        client = PureEnergie(host="example.com", session=session)
+        with pytest.raises(PureEnergieMeterError):
+            assert await client.request("test")
+
+
+@pytest.mark.asyncio
+async def test_http_error400(aresponses):
+    """Test HTTP 404 response handling."""
+    aresponses.add(
+        "example.com",
+        "/meter/now",
+        "GET",
+        aresponses.Response(text="Give me energy!", status=404),
+    )
+
+    async with aiohttp.ClientSession() as session:
+        client = PureEnergie(host="example.com", session=session)
+        with pytest.raises(PureEnergieMeterError):
+            assert await client.request("test")
+
+
+@pytest.mark.asyncio
+async def test_http_error500(aresponses):
+    """Test HTTP 500 response handling."""
+    aresponses.add(
+        "example.com",
+        "/meter/now",
+        "GET",
+        aresponses.Response(
+            body=b'{"status":"nok"}',
+            status=500,
+        ),
+    )
+
+    async with aiohttp.ClientSession() as session:
+        client = PureEnergie(host="example.com", session=session)
+        with pytest.raises(PureEnergieMeterError):
+            assert await client.request("test")
+
+
+@pytest.mark.asyncio
+async def test_no_success(aresponses):
+    """Test a message without a success message throws."""
+    aresponses.add(
+        "example.com",
+        "/meter/now",
+        "GET",
+        aresponses.Response(
+            status=200,
+            text='{"message": "no success"}',
+        ),
+    )
+
+    async with aiohttp.ClientSession() as session:
+        client = PureEnergie(host="example.com", session=session)
+        with pytest.raises(PureEnergieMeterError):
+            assert await client.request("test")

102e3921e7f8da4b378adcac1ca401b1763f769c
Klaas Schoute <klaas_schoute@hotmail.com>
2024-11-11 23:39:56 +0100

    Use syrupy for model testing (#830)
    * Update dependencies: bump Poetry to 1.8.4 and add Syrupy 4.7.2
    
    * Add snapshot tests for Device and SmartBridge models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,13 +1,18 @@
 """Test the models."""
 
 from aresponses import ResponsesMockServer
+from syrupy.assertion import SnapshotAssertion
 
 from gridnet import Device, GridNet, SmartBridge
 
 from . import load_fixtures
 
 
-async def test_device(aresponses: ResponsesMockServer, gridnet_client: GridNet) -> None:
+async def test_device(
+    aresponses: ResponsesMockServer,
+    snapshot: SnapshotAssertion,
+    gridnet_client: GridNet,
+) -> None:
     """Test request from the device - Device object."""
     aresponses.add(
         "127.0.0.1",
@@ -19,17 +24,13 @@ async def test_device(aresponses: ResponsesMockServer, gridnet_client: GridNet) 
         ),
     )
     device: Device = await gridnet_client.device()
-    assert device
-    assert device.n2g_id == "84df:0c11:9999:3795"
-    assert device.model == "SBWF3102"
-    assert device.batch == "SBP-HMX-210318"
-    assert device.firmware == "1.6.16"
-    assert device.hardware == 1
-    assert device.manufacturer == "NET2GRID"
+    assert device == snapshot
 
 
 async def test_smartbridge(
-    aresponses: ResponsesMockServer, gridnet_client: GridNet
+    aresponses: ResponsesMockServer,
+    snapshot: SnapshotAssertion,
+    gridnet_client: GridNet,
 ) -> None:
     """Test request from the device - SmartBridge object."""
     aresponses.add(
@@ -42,7 +43,4 @@ async def test_smartbridge(
         ),
     )
     smartbridge: SmartBridge = await gridnet_client.smartbridge()
-    assert smartbridge
-    assert smartbridge.power_flow == 338
-    assert smartbridge.energy_consumption_total == 17762.1
-    assert smartbridge.energy_production_total == 21214.6
+    assert smartbridge == snapshot
